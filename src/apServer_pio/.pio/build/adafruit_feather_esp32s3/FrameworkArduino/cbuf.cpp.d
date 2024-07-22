b8566a3617d40fdcd57faa61223d74ffe2d4fb4b
Marvic1130 <marvic1130@gmail.com>
2024-07-22 23:05:57 +0900

    Recreate Project files

--- src/apServer_pio/.pio/build/adafruit_feather_esp32s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/adafruit_feather_esp32s3/FrameworkArduino/cbuf.cpp.o: \
- /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- src/apServer_pio/.pio/build/adafruit_feather_esp32s3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/adafruit_feather_esp32s3/FrameworkArduino/libb64/cencode.c.o: \
- /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
