45bf47c5bd5d98a64f5c5b63bee2c49ab7059c84
Marvic1130 <marvic1130@gmail.com>
2024-07-22 22:30:06 +0900

    Create Project

---
+++ src/apServer_pio/.pio/build/adafruit_feather_esp32s3/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/adafruit_feather_esp32s3/FrameworkArduino/firmware_msc_fat.c.o: \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
---
+++ src/apServer_pio/.pio/build/adafruit_feather_esp32s3/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/adafruit_feather_esp32s3/FrameworkArduino/stdlib_noniso.c.o: \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_system/include/esp_system.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_err.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_compiler.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_attr.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_bit_defs.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_idf_version.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_hw_support/include/esp_mac.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_hw_support/include/esp_chip_info.h \
+ /Users/admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_hw_support/include/esp_random.h
